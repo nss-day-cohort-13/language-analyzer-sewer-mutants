ccb5024e60e1fa5bba2fa47e61c95d1dc4286a21
Mark Easterling <marke.easterling@gmail.com>
2016-07-20 08:32:10 -0500

    initial tests for sentiment

---
+++ Test_Sentiment_Analyzer.py
@@ -0,0 +1,23 @@
+import unittest
+#import (SENTIMENT FILE)
+
+class TestSentiment(unittest.TestCase):
+
+	def test_seniment_input_is_a_list(self):
+		pass
+
+	def test_sentiment_comprehension_returns_list_of_numbers_for_each_sentiment(self):
+		pass
+
+	def test_list_of_numbers_converted_to_dictionary_for_each_sentiment(self):
+		pass
+
+	def test_each_sentiments_dictionary_is_combined(self):
+		pass
+
+
+
+	if __name__ == '__main__':
+  unittest.main()
+
+
