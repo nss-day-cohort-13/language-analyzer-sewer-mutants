10d17d03fdef958b9e8bab043806ec699f7fc3d2
Castle Crawford <nathanscottcrawford@gmail.com>
2016-07-21 15:43:49 -0500

    behavior test and logic near completion

--- lexicon.py
+++ lexicon.py
@@ -1,34 +1,16 @@
 import nltk
 
-lexicon = [
-  {
-    "sentiment":
-    {
-      "positive": [("happy", 1),("nice", 1)],
-      "negative": [("terrible", 1),("bad", 1)],
-      "neutral": [("okay", 1)]
-    },
-    "domain":
-    {
-      "financial": [],
-      "behavioral": [],
-      "scientific": [],
-      "educational": [],
-      "politics": [],
-      "relationships": []
-    },
-    "behavior":
-    {
-      "aggressive": [],
-      "passive": [],
-      "mentoring": [],
-      "inquisitive": [],
-      "transaction": []
-    }
-  }
-]
+aggressive_behavior = {"hell":1, "hammer":1, "stomp":1, "ugly":1, "!":1}
+passive_behavior = {"Whatever":1, "meh":1, "fine":1, ".":1}
+# mentoring: {("were": 1:("you": 1:("would": 1:("handle": 1:("this": 1:("way": 1]:}
 
-"Friendship is like money, easier made than kept."
+excited_behavior = {"OMG":1, "!":1, "love":1, "chocolate":1, "!":1}
+inquisitive_behavior = {"Do":1, "even":1, "?":1}
+# "confident": {("my": 1:("team": 1:("will": 1:("defiinitely": 1:("have": 1:("report": 1:("on": 1:("desk": 1:("afternoon": 1]
 
 
-print(lexicon[0]["sentiment"]["positive"])
+
+
+# print(lexicon[0]["sentiment"]["positive"])
+
+
