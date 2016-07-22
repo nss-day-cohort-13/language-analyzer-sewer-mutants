0eb2c4f2c7354cb64c758aefe3a8d11c7b279e55
Castle Crawford <nathanscottcrawford@gmail.com>
2016-07-21 19:01:01 -0500

    all behavior tests currently passing

--- Test_Behavior_Analyzer.py
+++ Test_Behavior_Analyzer.py
@@ -6,21 +6,24 @@ class TestBehaviorAnalyzer(unittest.TestCase):
 
     @classmethod
     def setUp(self):
-        self.behavior_analyzer = Behave()
+        # self.behavior_analyzer = Behave()
         self.test_list_aggressive = ["Get", "the", "hell", "out", ",", "before", "I" ,"hammer", "stomp", "your", "ugly", "face", "in", "!"]
         self.test_list_passive = ["Whatever", "it's", "meh", "they", "taste", "fine", "."]
         self.test_list_excited = ["OMG","!", "I", "love", "chocolate", "!"]
         self.test_list_inquisitive = ["Do", "you", "even", "lift", "bro", "?"]
 
-        # self.test_dict_aggressive= {"aggresive": 5}
-        # self.test_dict_passive= {"passive": 4}
-        # self.test_dict_excited= {"excited": 5}
-        # self.test_dict_inquisitive = {"inquisitive": 3}
+        self.test_dict_aggressive= {"aggressive": 5}
+        self.test_dict_passive= {"passive": 4}
+        self.test_dict_excited= {"excited": 5}
+        self.test_dict_inquisitive = {"inquisitive": 3}
 
         self.agro_nums= [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1]
         self.passive_nums= [1, 0, 1, 0, 0, 1, 1]
         self.excited_nums= [1,1,0,1,1,1]
         self.inquisitive_nums= [1, 0, 1, 0, 0, 1]
+
+        self.behavior_analyzer= Behave(self.agro_nums, self.passive_nums, self.excited_nums, self.inquisitive_nums)
+
 
     def test_behavior_input_is_a_list(self):
         self.assertIsInstance(self.test_list_aggressive,list)
@@ -31,6 +34,7 @@ class TestBehaviorAnalyzer(unittest.TestCase):
 
 
     def test_behavior_comprehension_returns_list_of_numbers_for_aggressive_behavior(self):
+        # self.behavior_analyzer = Behave(self.test_list_aggressive)
         self.assertEqual(self.behavior_analyzer.assign_aggressive_values(self.test_list_aggressive),[0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1])
 
     def test_behavior_comprehension_returns_list_of_numbers_for_passive_behavior(self):
@@ -55,6 +59,18 @@ class TestBehaviorAnalyzer(unittest.TestCase):
 
     def test_list_of_numbers_converted_to_dictionary_for_inquisitive_behavior(self):
         self.assertEqual(self.behavior_analyzer.inquisitive_dictionary(self.inquisitive_nums), {"inquisitive": 3})
+
+    def testIt1(self):
+        self.assertEqual(self.behavior_analyzer.aggressiveDict, self.test_dict_aggressive)
+
+    def testIt2(self):
+        self.assertEqual(self.behavior_analyzer.passiveDict, self.test_dict_passive)
+
+    def testIt3(self):
+        self.assertEqual(self.behavior_analyzer.excitedDict, self.test_dict_excited)
+
+    def testIt4(self):
+        self.assertEqual(self.behavior_analyzer.inquisitiveDict, self.test_dict_inquisitive)
 
 
 
